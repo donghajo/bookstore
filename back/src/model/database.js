afac87647c28a9217d40072141e2830a83b59305
jodongha <jodongrami@gmail.com>
2023-01-31 13:45:13 +0900

    ADD : database setting

---
+++ back/src/model/database.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+const dbConfig = require('../config/db.config.json');
+
+const pool = mysql.createPool({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    port: dbConfig.port,
+});
+
+pool.getConnection(function (err, connenction) {
+    if (err) {
+        console.log("DB connect fail : ", err);
+    } else {
+        console.log("DB connect success");
+        connenction.release();
+    }
+});
+
+
+
+module.exports = pool.promise();
