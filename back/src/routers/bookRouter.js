540f3e77ab31db59407b1226cbbba7a0e152d381
jodongha <jodongrami@gmail.com>
2023-02-09 00:23:38 +0900

    ADD : 도서 주문 기능

--- back/src/routers/bookRouter.js
+++ back/src/routers/bookRouter.js
@@ -4,5 +4,6 @@ const bookController = require('../controllers/bookController');
 
 router.get('/', bookController.readBookList);
 router.get('/book/:pid', bookController.readBook);
+router.post('/book/:pid', bookController.orderBook);
 
 module.exports = router;
